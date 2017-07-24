067a93fa58f5389f9141ee0ebb7a00dce017f82a
hecd <hecd744@gmail.com>
2017-07-24 21:41:24 +0200

    Installs dkms in devtools/install/virtualbox.sh

--- devtools/install/virtualbox.sh
+++ devtools/install/virtualbox.sh
@@ -9,4 +9,4 @@ wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 
 sudo apt-get update
-sudo apt-get install -y virtualbox-5.1
+sudo apt-get install -y virtualbox-5.1 dkms
