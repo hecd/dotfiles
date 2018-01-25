c3290560a2f92d197c7dd37ec5709931cd9d1dde
hecd <hecd744@gmail.com>
2018-01-25 21:48:17 +0100

    Moves data to ring directories.

--- setup.sh
+++ setup.sh
@@ -10,6 +10,7 @@ fi
 export DOTFILES_DIR=`dirname $(readlink -f $0)`
 # Source environment so environment variables such as $DOTFILES_DIR can be used in install scripts.
 source "$DOTFILES_DIR/base/system/env"
+mkdir -p "$RINGS"/{0..5}
 
 # Create symlinks.
 ln -sfv -T "$DOTFILES_BASE_DIR/zsh/zshrc" ~/.zshrc
@@ -18,12 +19,9 @@ ln -sfv -T "$DOTFILES_BASE_DIR/zsh/zshenv" ~/.zshenv
 mkdir -p ~/.vim
 ln -sfv -T "$DOTFILES_BASE_DIR/vim/vimrc" ~/.vimrc
 ln -sfv -T "$DOTFILES_BASE_DIR/vim/colors" ~/.vim/colors
-
 ln -sfv -T "$DOTFILES_BASE_DIR/git/gitconfig" ~/.gitconfig
 ln -sfv -T "$DOTFILES_BASE_DIR/git/gitignore" ~/.gitignore
-
 ln -sfv -T "$DOTFILES_BASE_DIR/tmux/tmux.conf" ~/.tmux.conf
-
 ln -sfv -T "$DOTFILES_BASE_DIR/rc/inputrc" ~/.inputrc
 ln -sfv -T "$DOTFILES_BASE_DIR/rc/procmailrc" ~/.procmailrc
 
