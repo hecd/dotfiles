50f51969b64a2e7ddddf0f2a0ee5c4fb5ae4cd6d
hecd <hecd744@gmail.com>
2019-01-06 18:24:41 +0100

    Removes base/ directory since it is no longer needed after devtools was deleted. Removes DOTFILES_BASE_DIR environment variable and all usages of it.

--- setup.sh
+++ setup.sh
@@ -3,26 +3,26 @@
 rm -rf ~/.vim
 
 # Source environment config, enabling use of environment variables in install scripts.
-source "`dirname $(readlink -f $0)`/base/system/env"
+source "`dirname $(readlink -f $0)`/system/env"
 
 # Create symlinks.
-ln -sfv -T "$DOTFILES_BASE_DIR/zsh/zshrc" ~/.zshrc
-ln -sfv -T "$DOTFILES_BASE_DIR/zsh/zshenv" ~/.zshenv
+ln -sfv -T "$DOTFILES_DIR/zsh/zshrc" ~/.zshrc
+ln -sfv -T "$DOTFILES_DIR/zsh/zshenv" ~/.zshenv
 
 mkdir -p ~/.vim
-ln -sfv -T "$DOTFILES_BASE_DIR/vim/vimrc" ~/.vimrc
-ln -sfv -T "$DOTFILES_BASE_DIR/vim/colors" ~/.vim/colors
-ln -sfv -T "$DOTFILES_BASE_DIR/git/gitconfig" ~/.gitconfig
-ln -sfv -T "$DOTFILES_BASE_DIR/git/gitignore" ~/.gitignore
-ln -sfv -T "$DOTFILES_BASE_DIR/tmux/tmux.conf" ~/.tmux.conf
-ln -sfv -T "$DOTFILES_BASE_DIR/rc/inputrc" ~/.inputrc
-ln -sfv -T "$DOTFILES_BASE_DIR/rc/procmailrc" ~/.procmailrc
-ln -sfv -T "$DOTFILES_BASE_DIR/rc/clipitrc" ~/.config/clipit/clipitrc
-ln -sfv -T "$DOTFILES_BASE_DIR/x11/xprofile" ~/.xprofile
+ln -sfv -T "$DOTFILES_DIR/vim/vimrc" ~/.vimrc
+ln -sfv -T "$DOTFILES_DIR/vim/colors" ~/.vim/colors
+ln -sfv -T "$DOTFILES_DIR/git/gitconfig" ~/.gitconfig
+ln -sfv -T "$DOTFILES_DIR/git/gitignore" ~/.gitignore
+ln -sfv -T "$DOTFILES_DIR/tmux/tmux.conf" ~/.tmux.conf
+ln -sfv -T "$DOTFILES_DIR/rc/inputrc" ~/.inputrc
+ln -sfv -T "$DOTFILES_DIR/rc/procmailrc" ~/.procmailrc
+ln -sfv -T "$DOTFILES_DIR/rc/clipitrc" ~/.config/clipit/clipitrc
+ln -sfv -T "$DOTFILES_DIR/x11/xprofile" ~/.xprofile
 
-source "$DOTFILES_BASE_DIR/install/locale.sh"
-source "$DOTFILES_BASE_DIR/install/apt.sh"
-source "$DOTFILES_BASE_DIR/install/oh-my-zsh.sh"
+source "$DOTFILES_DIR/install/locale.sh"
+source "$DOTFILES_DIR/install/apt.sh"
+source "$DOTFILES_DIR/install/oh-my-zsh.sh"
 
 [ -x "$EXTRA_DIR/setup.sh" ] && source $EXTRA_DIR/setup.sh
 
