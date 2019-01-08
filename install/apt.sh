7c87a6fe96050bef856f4c99caa723e9839e7021
hecd <hecd744@gmail.com>
2019-01-08 19:20:58 +0100

    Adds package fonts-powerline containing font used for new theme.

--- install/apt.sh
+++ install/apt.sh
@@ -1,2 +1,2 @@
 sudo apt-get -y update
-sudo apt-get -y install curl python-dev python3-dev vim-gtk git git-gui zsh build-essential cmake tmux wget tree iptables-persistent libguestfs-tools scdaemon gnupg2 yubioath-desktop xclip qemu-kvm bridge-utils virt-manager davfs2 net-tools ranger clipit zsh-syntax-highlighting
+sudo apt-get -y install curl python-dev python3-dev vim-gtk git git-gui zsh build-essential cmake tmux wget tree iptables-persistent libguestfs-tools scdaemon gnupg2 yubioath-desktop xclip qemu-kvm bridge-utils virt-manager davfs2 net-tools ranger clipit zsh-syntax-highlighting fonts-powerline
