a5ab00d083fe3fd12eb6dc854c93e750fc4b43bc
hecd <hecd744@gmail.com>
2020-10-02 22:14:21 +0200

    Adds support for oh my zsh plugins zsh-autosuggestions zsh-syntax-highlighting.

--- install/oh-my-zsh.sh
+++ install/oh-my-zsh.sh
@@ -2,3 +2,6 @@
 rm -rf ~/.oh-my-zsh
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 sudo chsh -s /bin/zsh $USER
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
