bd58fceb89d509ed378cc1da05e206bdc220bfa2
hecd <hecd744@gmail.com>
2020-10-03 23:20:18 +0200

    Moves and breaks apart files in order to provide a better structure for OS specific files, effectively allowing dotfiles to be used for multiple distros (not there yet).

--- setup.sh
+++ setup.sh
@@ -1,28 +1,15 @@
 #!/bin/bash
 
-rm -rf ~/.vim
-
 # Source environment config, enabling use of environment variables in install scripts.
 source "`dirname $(readlink -f $0)`/system/env"
 
-# Create symlinks.
-ln -sfv -T "$DOTFILES_DIR/zsh/zshrc" ~/.zshrc
-ln -sfv -T "$DOTFILES_DIR/zsh/zshenv" ~/.zshenv
-
+rm -rf ~/.vim
 mkdir -p ~/.vim
-ln -sfv -T "$DOTFILES_DIR/vim/vimrc" ~/.vimrc
-ln -sfv -T "$DOTFILES_DIR/vim/colors" ~/.vim/colors
-ln -sfv -T "$DOTFILES_DIR/git/gitconfig" ~/.gitconfig
-ln -sfv -T "$DOTFILES_DIR/git/gitignore" ~/.gitignore
-ln -sfv -T "$DOTFILES_DIR/tmux/tmux.conf" ~/.tmux.conf
-ln -sfv -T "$DOTFILES_DIR/i3/config" ~/.config/i3/config
-ln -sfv -T "$DOTFILES_DIR/x11/xprofile" ~/.xprofile
 
-source "$DOTFILES_DIR/install/locale.sh"
-source "$DOTFILES_DIR/install/apt.sh"
-source "$DOTFILES_DIR/install/oh-my-zsh.sh"
-
-[ -x "$EXTRA_DIR/setup.sh" ] && source $EXTRA_DIR/setup.sh
+source "$DOTFILES_DIR/install-scripts/common.sh"
+# TODO: execute distro specific package install "$DOTFILES_DIR/install-scripts/{distro}/install-packages.sh"
+source "$DOTFILES_DIR/install-scripts/debian/install-packages.sh"
+source "$DOTFILES_DIR/install-scripts/create-symlinks.sh"
 
 echo
 echo
