3e25ce3de3813b85ae931573e850ae5341b1bf34
hecd <hecd744@gmail.com>
2021-06-15 21:55:28 +0200

    Removes cursor key bindings in vscode in order to force vim use ;-).

--- install-scripts/create-symlinks.sh
+++ install-scripts/create-symlinks.sh
@@ -11,5 +11,6 @@ ln -sfv -T "$DOTFILES_DIR/tmux/tmux.conf" ~/.tmux.conf
 #ln -sfv -T "$DOTFILES_DIR/i3/config" ~/.config/i3/config
 ln -sfv -T "$DOTFILES_DIR/x11/xprofile" ~/.xprofile
 ln -sfv -T "$DOTFILES_DIR/vscode/settings.json" ~/.config/Code\ -\ OSS/User/settings.json
+ln -sfv -T "$DOTFILES_DIR/vscode/keybindings.json" ~/.config/Code\ -\ OSS/User/keybindings.json
 # TODO: Check for presence instead of assuming code OSS is used instead of VS Code (Microsoft branded)
 #ln -sfv -T "$DOTFILES_DIR/vscode/settings.json" ~/.config/Code/settings.json
