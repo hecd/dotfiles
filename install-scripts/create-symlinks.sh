9bd03343e6dd4e505fb18e8b3e16555b8c55d1b6
hecd <hecd744@gmail.com>
2021-06-15 21:14:47 +0200

    create-symlinks now creates symlink for code-oss instead of MS branded product.

--- install-scripts/create-symlinks.sh
+++ install-scripts/create-symlinks.sh
@@ -10,4 +10,6 @@ ln -sfv -T "$DOTFILES_DIR/tmux/tmux.conf" ~/.tmux.conf
 # TODO: Fix i3 for arch
 #ln -sfv -T "$DOTFILES_DIR/i3/config" ~/.config/i3/config
 ln -sfv -T "$DOTFILES_DIR/x11/xprofile" ~/.xprofile
-ln -sfv -T "$DOTFILES_DIR/vscode/user/settings.json" ~/.config/Code\ -\ OSS/User/settings.json
+ln -sfv -T "$DOTFILES_DIR/vscode/settings.json" ~/.config/Code\ -\ OSS/User/settings.json
+# TODO: Check for presence instead of assuming code OSS is used instead of VS Code (Microsoft branded)
+#ln -sfv -T "$DOTFILES_DIR/vscode/settings.json" ~/.config/Code/settings.json
