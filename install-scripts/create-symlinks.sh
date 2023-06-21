85d23763abc801ce63ad137eb6a9310ff9752b9b
hecd <hecd744@gmail.com>
2023-06-21 23:06:39 +0200

    Symlinks are now created to VS Code (MS branded) instead of OSS version.

--- install-scripts/create-symlinks.sh
+++ install-scripts/create-symlinks.sh
@@ -10,7 +10,10 @@ ln -sfv -T "$DOTFILES_DIR/tmux/tmux.conf" ~/.tmux.conf
 # TODO: Fix i3 for arch
 #ln -sfv -T "$DOTFILES_DIR/i3/config" ~/.config/i3/config
 ln -sfv -T "$DOTFILES_DIR/x11/xprofile" ~/.xprofile
-ln -sfv -T "$DOTFILES_DIR/vscode/settings.json" ~/.config/Code\ -\ OSS/User/settings.json
-ln -sfv -T "$DOTFILES_DIR/vscode/keybindings.json" ~/.config/Code\ -\ OSS/User/keybindings.json
-# TODO: Check for presence instead of assuming code OSS is used instead of VS Code (Microsoft branded)
-#ln -sfv -T "$DOTFILES_DIR/vscode/settings.json" ~/.config/Code/settings.json
+
+# TODO: Check for presence instead of assuming code OSS is used in contract to VS Code (Microsoft branded)
+#ln -sfv -T "$DOTFILES_DIR/vscode/settings.json" ~/.config/Code\ -\ OSS/User/settings.json
+#ln -sfv -T "$DOTFILES_DIR/vscode/keybindings.json" ~/.config/Code\ -\ OSS/User/keybindings.json
+
+ln -sfv -T "$DOTFILES_DIR/vscode/keybindings.json" ~/.config/Code/User/keybindings.json
+ln -sfv -T "$DOTFILES_DIR/vscode/settings.json" ~/.config/Code/User/settings.json
